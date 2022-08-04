519123e30be312be979c5eb27ecddb4c7e8364cf
ezefaz <ezefaz@gmail.com>
2022-08-04 01:08:52 -0300

    Comment section problem solved

--- Client/src/api/index.js
+++ Client/src/api/index.js
@@ -16,7 +16,7 @@ export const fetchPostsBySearch = (searchQuery) => API.get(`/posts/search?search
 export const fetchPostsByCreator = (name) => API.get(`/posts/creator?name=${name}`);
 export const createPost = (newPost) => API.post('/posts', newPost);
 export const likePost = (id) => API.patch(`/posts/${id}/likePost`, likePost);
-export const comment = (value, id) => API.patch(`/posts/${id}/commentPost`, { value });
+export const comment = (value, id) => API.post(`/posts/${id}/commentPost`, { value });
 export const updatePost = (id, updatedPost) => API.patch(`/posts/${id}`, updatedPost); 
 export const deletePost = (id) => API.delete(`/posts/${id}`, deletePost);
 
